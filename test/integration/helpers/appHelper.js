6fd805af779e54fb2860349c654b808e8174680d
Justin Hollingsworth <justin.hollingsworth@steelthread.com>
2018-03-27 15:18:01 -0400

    remove grunt

--- test/integration/helpers/appHelper.js
+++ test/integration/helpers/appHelper.js
@@ -144,7 +144,6 @@ module.exports = {
       environment: process.env.TEST_ENV
     });
     options.hooks = options.hooks || {};
-    options.hooks.grunt = options.hooks.grunt || false;
 
     Sails().lift(options, function(err, sails) {
       if (err) {
