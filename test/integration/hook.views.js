0e7df33dd5caf10e23cf53e97caabc7b1d89e75e
Justin Hollingsworth <justin.hollingsworth@steelthread.com>
2018-03-27 15:12:32 -0400

    remove i18n

--- test/integration/hook.views.js
+++ test/integration/hook.views.js
@@ -65,56 +65,6 @@ describe('hooks :: ', function() {
       });
 
     });
-
-    describe('using renderView (with i18n disabled)', function () {
-
-      var appName = 'testApp';
-
-      before(function(done) {
-        // Clear the require cache of i18n
-        _.each(Object.keys(require.cache), function(path) {
-          if (path.match(/\/node_modules\/i18n\//)) {
-            delete require.cache[path];
-          }
-        });
-
-        appHelper.lift({
-          verbose: false,
-          hooks: {
-            i18n: false
-          }
-        }, function(err, sails) {
-          if (err) {
-            throw new Error(err);
-          }
-          sailsprocess = sails;
-          setTimeout(done, 100);
-        });
-      });
-
-      after(function(done) {
-        sailsprocess.lower(function() {
-          setTimeout(done, 100);
-        });
-
-      });
-
-
-      it('should respond to a get request to localhost:1342 with welcome page', function(done) {
-
-        httpHelper.testRoute('get', 'renderView', function(err, response) {
-          if (err) {
-            return done(new Error(err));
-          }
-
-          assert(response.body.indexOf('not found') < 0);
-          assert(response.body.indexOf('<!-- Default home page -->') > -1);
-          done();
-        });
-      });
-
-    });
-
   });
 
 });
